c46bf7e1066115da83c4ae93f6256f060518adf4
Aikenpil <78743601+Aikenpil@users.noreply.github.com>
2022-01-19 17:24:25 -0300

    Add files via upload

---
+++ Trabalho_3/src/com/company/Conjunto.java
@@ -0,0 +1,27 @@
+/**
+ * @file   Conjunto.java
+ * @brief  Arquivo dedicado à classe abstrata Conjunto.
+ * @author Gustavo Rangel Bandeira Cauduro
+ * @date   2022-01-15
+ */
+
+package com.company;
+
+public abstract class Conjunto<T> {
+    public abstract Boolean contemElemento(T elemento);
+    Conjunto<T> adicionarElemento(T elemento) {
+        return new ConjuntoElemento<>(elemento, this);
+    }
+    Conjunto<T> uniao(Conjunto<T> conjunto ) {
+        return new Uniao<>(this, conjunto);
+    }
+    Conjunto<T> intersecao(Conjunto<T> conjunto) {
+        return new Intersecao<>(this, conjunto);
+    }
+    Conjunto<T> diferenca(Conjunto<T> conjunto) {
+        return new Diferenca<>(this, conjunto);
+    }
+    Conjunto<T> complemento() {
+        return new Complemento<>(this);
+    }
+}
