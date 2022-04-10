d5bfc5f7906a183c55c856889f7104518949b2bc
Gustavo Rangel <78743601+Aikenpil@users.noreply.github.com>
2022-04-10 17:46:08 -0300

    Add files via upload

---
+++ Trabalho_3_C#/Trabalho_3_CSharp/Conjunto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho_3_CSharp
+{
+    public abstract class Conjunto<T> {
+
+        public abstract Boolean contemElemento(T elemento);
+        public Conjunto<T> adicionarElemento(T elemento) {
+            return new ConjuntoElemento<T>(elemento, this);
+        }
+        public Conjunto<T> uniao(Conjunto<T> conjunto) {
+            return new Uniao<T>(this, conjunto);
+        }
+        public Conjunto<T> intersecao(Conjunto<T> conjunto) {
+            return new Intersecao<T>(this, conjunto);
+        }
+        public Conjunto<T> diferenca(Conjunto<T> conjunto) {
+            return new Diferenca<T>(this, conjunto);
+        }
+        public Conjunto<T> complemento() {
+            return new Complemento<T>(this);
+        }
+    }
+}
---
+++ Trabalho_3_C#/Trabalho_3_CSharp/ConjuntoVazio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho_3_CSharp
+{
+    public class ConjuntoVazio<T> : Conjunto<T> {
+
+        public ConjuntoVazio() {
+        }
+
+        public override Boolean contemElemento(T elemento) {
+            return false;
+        }
+
+    }
+}
